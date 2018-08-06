50b4f112c8b59dabf721cd3b83fc40ca7d0c0fb1
Bennett <bennettcheung@yahoo.com>
2018-08-06 13:07:05 -0700

    Setup the initial layout on the storyboard

---
+++ TwoPlayerMathGame/Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  TwoPlayerMathGame
+//
+//  Created by Bennett on 2018-08-06.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Player : NSObject
+
+@end
---
+++ TwoPlayerMathGame/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  TwoPlayerMathGame
+//
+//  Created by Bennett on 2018-08-06.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
